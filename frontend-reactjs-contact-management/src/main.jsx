b96c15ba0482e6da9a7de1f23c6082d3a5e805f4
Salim P Ramadhan <salimpurnamaramadhan.ti.uk.1050@gmail.com>
2025-05-31 20:19:42 +0700

    02:07:10 Detail Contact Page

--- frontend-reactjs-contact-management/src/main.jsx
+++ frontend-reactjs-contact-management/src/main.jsx
@@ -7,6 +7,10 @@ import UserLogin from "./components/User/UserLogin";
 import DashboardLayout from "./components/DashboardLayout";
 import UserProfile from "./components/User/UserProfile";
 import UserLogout from "./components/User/UserLogout";
+import ContactCreate from "./components/Contact/ContactCreate";
+import ContactList from "./components/Contact/ContactList";
+import ContactEdit from "./components/Contact/ContactEdit";
+import ContactDetail from "./components/Contact/ContactDetail";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -17,9 +21,16 @@ createRoot(document.getElementById("root")).render(
           <Route path="/login" element={<UserLogin />}></Route>
         </Route>
         <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route path="contacts" element={<div>Contacts</div>} />
-          <Route path="users/profile" element={<UserProfile />} />
-          <Route path="users/logout" element={<UserLogout />} />
+          <Route path="users">
+            <Route path="profile" element={<UserProfile />} />
+            <Route path="logout" element={<UserLogout />} />
+          </Route>
+          <Route path="contacts">
+            <Route index element={<ContactList />} />
+            <Route path="create" element={<ContactCreate />} />
+            <Route path=":id/" element={<ContactDetail />} />
+            <Route path=":id/edit" element={<ContactEdit />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
